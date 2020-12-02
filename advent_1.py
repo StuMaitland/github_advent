3799f835d2987e150cbf289f6c4de101caffa6da
Stuart Maitland <stuart@mediwikis.com>
2020-12-02 17:13:47 +0000

    Tidied up the first entry a bit

--- advent_1.py
+++ advent_1.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
-nos=pd.read_csv('advent_data_1.csv')
+nos = pd.read_csv('advent_data_1.csv')
 
-#print(nos)
 
+# This is very messy, could probably do this with transposed arrays, but meh.
+# Section 1
+for number in nos.values:
+    for number2 in nos.values:
+        add = number+number2
+        if add == 2020:
+            print(number*number2)
+
+# Section 2
 for number in nos.values:
     for number2 in nos.values:
         for number3 in nos.values:
-            add=number+number2+number3
-            if add==2020:
+            add = number+number2+number3
+            if add == 2020:
                 print(number*number2*number3)
